056161bb0ae3bc306f3a7b89f6f86ffbeba3acb6
Aveson <avesoncarvalho25@gmail.com>
2023-11-16 13:09:52 +0000

    fixied on click, added more comments

--- javascripts/index.js
+++ javascripts/index.js
@@ -27,24 +27,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Displaying products
 function productDisplay() {
-	fetch("https://fakestoreapi.com/products?limit=50")
+	fetch("https://fakestoreapi.com/products")
 		.then((data) => data.json())
 		.then((data) => {
-			console.log(data);
 			const productBox = document.querySelector(".catalogue-box");
 			productBox.textContent = "";
 			for (let index = 0; index < data.length; index++) {
 				productBox.innerHTML += `<div class="product-item ${index + 1}"><img draggable="false" (dragstart)="false;" class="product-info product-image rounded-top" src="${data[index].image}" alt="thumbnail image of product"/><span class="product-info text-dark bg-white">${data[index].title}</span><button type="button" class="btn btn-warning">Add 1 To Cart</button></div>`;
 			}
+
+			// Onclick function for the displayed items
+			const productInfo = document.querySelectorAll(".product-info");
+			productInfo.forEach((item) => {
+				item.addEventListener("click", function (event) {
+					const productID = item.parentElement.classList[1];
+					displayInfo(productID);
+				});
+			});
 		});
 }
-// Display certain product's details
-document.querySelectorAll(".product-info").forEach((item) => {
-	item.onclick = () => {
-		console.log("hi");
-		const productID = item.parentElement.classList[1];
-		console.log(productID);
-	};
-});
+
 // Display
 productDisplay();
+
+// Display certain product's details (onclick)
+function displayInfo(ID) {
+	fetch(`https://fakestoreapi.com/products/${ID}`)
+		.then((data) => data.json())
+		.then((data) => {
+			console.log(data);
+		});
+}
