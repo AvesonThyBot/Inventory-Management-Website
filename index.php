90619c5d9b0c800a0ea6e72facbb7a71f35afb0d
Aveson <avesoncarvalho25@gmail.com>
2023-11-22 19:35:21 +0000

    added profile page, added suitable code and files for profile

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
-    require "config/database.php";
+require "config/database.php";
 
-    // send to account
-    if (count($_COOKIE) <= 0) {
-            header("Location:account.php");
-        }
-    
-    // Get user data
-    $user_id = $_COOKIE['user_id'];
-    $sql = "SELECT * FROM users WHERE user_id = $user_id";
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result);        
+// send to account
+if (count($_COOKIE) <= 0) {
+    header("Location:account.php");
+}
+
+// Get user data
+$user_id = $_COOKIE['user_id'];
+$sql = "SELECT * FROM users WHERE user_id = $user_id";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,11 +23,12 @@
     <link rel="shortcut icon" href="images/inventory.png" type="image/png">
     <title>Catalogue - Inventory</title>
 </head>
+
 <body class="bg-dark">
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark" >
+    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
         <div class="container-fluid">
-            <img class="navbar-brand img-fluid" src="/images/inventory.png" style="height:50px;" alt="logo" draggable="false"/>
+            <img class="navbar-brand img-fluid" src="/images/inventory.png" style="height:50px;" alt="logo" draggable="false" />
             <!-- Button when compressed -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -50,7 +52,7 @@
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end w-100 mw-100">
                         <?php if (isset($_COOKIE["is_logged_in"])) { ?>
-                            <li><p class="dropdown-item"><?php echo $row['first_name'];?></p></li>
+                            <li><a class="dropdown-item" href="profile.php"><?php echo $row['first_name']; ?></a></li>
                             <li>
                                 <hr class="dropdown-divider" />
                             </li>
@@ -70,12 +72,12 @@
 
     <!-- Catalogue Section -->
     <section class="catalogue-section text-white">
-        <h1>Hello, <?php echo $row['first_name'];?></h1>
+        <h1>Hello, <?php echo $row['first_name']; ?></h1>
         <div class="product-list text-white">
-			<h2>Products</h2>
-			<div class="catalogue-box">
-			</div>
-		</div>
+            <h2>Products</h2>
+            <div class="catalogue-box">
+            </div>
+        </div>
 
     </section>
 
@@ -89,4 +91,5 @@
     <script src="javascripts/index.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
-</html> 
+
+</html>
