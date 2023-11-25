de6a4bc254e03bbd23e1154c020e6d15ca94d84b
Aveson <avesoncarvalho25@gmail.com>
2023-11-25 19:39:21 +0000

    changed sql data

--- users.sql
+++ users.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2023 at 04:28 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Nov 25, 2023 at 08:35 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,17 +33,19 @@ CREATE TABLE `users` (
   `last_name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password_text` varchar(80) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `email`, `password_text`) VALUES
-(1, 'a', 'b', 'c@gmail.com', '$2y$10$8mrt1doYXwxMK8kkF.mgX.aEk13thcyZ6qwsOeluR3c1o.MWCaPVu'),
-(2, 'a', 'b', 'c@gmail.com', '$2y$10$jcJp2nCw8Zhq1GRdxUBfCOeYPHobLs0571x18ERg0ompnd3YffXTu'),
-(3, '2', '1', 'ajnkhdf@Gmail.com', '$2y$10$xL49y0Rs/9BUiO48.r4.xe4fCINczvYvsByYrjWf9hTosBSbtjRSe'),
-(4, '2', '1', 'ajnkhdf@Gmail.com', '$2y$10$oOvdS5ky6GgteQHgg62L7.UJbm1YYzVe8jdn46cCjG9RAXhdBWclm');
+(1, 'Aveson', 'b', 'a@gmail.com', 'a'),
+(2, '2', '1', 'ajnkhdf@Gmail.com', '$2y$10$xL49y0Rs/9BUiO48.r4.xe4fCINczvYvsByYrjWf9hTosBSbtjRSe'),
+(3, 'test', 'lasttest', 'test@email.com', '$2y$10$0PMhlneF7vr3DyzIvXc9.u/Y.ppQi/4I.UMrX0ETGuJqSeda0rlNW'),
+(4, 'test', 'teste', 'testes@gmail.com', '$2y$10$MpAuCSlLhdCj6/tHMFgJEulxIB4IQhwfMBjZhuBFhFP02MQCiraPm'),
+(5, 'qwerty', 'qwerty', 'good@email.com', '$2y$10$USOtGhilHwG9R9IApXd8x.XNAsZC54b9B.b74CSrdJUtyJNWWDdDW'),
+(6, 'a', 'a', 'aaaa@gmail.com', '$2y$10$zzI/yN0649BHeCFU63vxw.eCXC6S1pL52j1RbTM39tEylxX3Gmak.');
 
 --
 -- Indexes for dumped tables
